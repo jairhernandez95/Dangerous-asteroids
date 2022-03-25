c91a73722e372b818a05e88c10e82754fdee058f
Jair Hernández <jair_097@hotmail.com>
2022-03-25 16:48:46 -0700

    Update main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// Generate API Key
+// Your API key for jairhdzp95@gmail.com is:
+
+// eehUavXZofKgxSW7ewrkWpqdQ7s0JsNI7Syryp8U
+// You can start using this key to make web service requests. Simply pass your key in the URL when making a web request. Here's an example:
+
+// https://api.nasa.gov/planetary/apod?api_key=eehUavXZofKgxSW7ewrkWpqdQ7s0JsNI7Syryp8U
+// For additional support, please contact us. When contacting us, please tell us what API you're accessing and provide the following account details so we can quickly find you:
+
+// Account Email: jairhdzp95@gmail.com
+// Account ID: f29bf9e4-8dd8-4574-9961-8c4abed295b8
