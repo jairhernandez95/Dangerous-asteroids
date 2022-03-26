eb91d6e227b28361acea086f3a9d3de4a19a5d66
Jair Hernández <jair_097@hotmail.com>
2022-03-25 19:07:24 -0700

    Update

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-// Generate API Key
-// Your API key for jairhdzp95@gmail.com is:
+let mainSection1 = document.getElementById("mainSection1");
+let mainSection2 = document.getElementById("mainSection2");
+let mainDivResults = document.getElementById("mainDivResults");
 
-// eehUavXZofKgxSW7ewrkWpqdQ7s0JsNI7Syryp8U
-// You can start using this key to make web service requests. Simply pass your key in the URL when making a web request. Here's an example:
-
-// https://api.nasa.gov/planetary/apod?api_key=eehUavXZofKgxSW7ewrkWpqdQ7s0JsNI7Syryp8U
-// For additional support, please contact us. When contacting us, please tell us what API you're accessing and provide the following account details so we can quickly find you:
-
-// Account Email: jairhdzp95@gmail.com
-// Account ID: f29bf9e4-8dd8-4574-9961-8c4abed295b8
+function searchAsteroids() 
+{
+    mainSection1.innerHTML = ``;
+    let dateInput = document.getElementById("dateInput").value
+    dateInput = dateInput.replace("/", "-");
+    let config = {
+    method: "get",
+    url: `https://api.nasa.gov/neo/rest/v1/feed?start_date=${dateInput}&end_date=${dateInput}&api_key=eehUavXZofKgxSW7ewrkWpqdQ7s0JsNI7Syryp8U`,
+    headers: {}
+  }
+    axios(config).then(response => 
+        {
+            let objects = response.data.near_earth_objects[dateInput]
+            console.log(objects)
+        }).catch(error)
+        {
+            console.log(error);
+        }
+}
