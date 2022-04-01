e78090b05478c7561eb7b2988a4c4fd0e2d5846b
Jair Hernández <jair_097@hotmail.com>
2022-04-01 14:16:49 -0700

    Update

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let auxiliarArray = [];
 
 function searchAsteroids() 
 {
+    auxiliarArray = []
     let dateInput = document.getElementById("dateInput").value
     dateInput = dateInput.replace("/", "-");
     let config = {
@@ -27,11 +28,36 @@ function searchAsteroids()
 
 function showAsteroids(array)
 {
-    header.setAttribute("position", "fixed")
+    header.style.height = "min-content"
     console.log(array);
+    mainDivSearch.setAttribute("class", "fixed-top");
+    mainDivSearch.style.color = "black";
+    mainDivSearch.style.backgroundColor= "white";
+    console.log(array);
+    let card = ``;
     for(let i = 0; i < array.length; i++)
     {
-        let card = `${array[i].name}`;
-        console.log(card);
+        let name = array[i].name;
+        let closeApproachDate = array[i].close_approach_data[0].close_approach_date_full;
+        let relativeVelocity = parseFloat(array[i].close_approach_data[0].relative_velocity.kilometers_per_second).toFixed(2);
+        let estimatedDiameterMetersMax =  array[i].estimated_diameter.meters.estimated_diameter_max.toFixed(2);
+        let estimatedDiameterMetersMin =  array[i].estimated_diameter.meters.estimated_diameter_min.toFixed(2);
+        // console.log(name);
+        // console.log(closeApproachDate);
+        // console.log(relativeVelocity);
+        // console.log(estimatedDiameterMetersMax);
+        // console.log(estimatedDiameterMetersMin);
+        card += `
+            <div class="card"  style="width: 18rem;">
+                <div class="card-body">
+                    <div class="titlesOfCard">
+                        <h2 class="card-title">Name: ${name}</h2>
+                        <h4 class="card-subtitle mb-2 text-muted">Close approach date: ${closeApproachDate}</h4>
+                        <h4>Relative velocity: ${relativeVelocity} km/s</h4>
+                        <h4>Estimated diameter: from ${estimatedDiameterMetersMin} meters to ${estimatedDiameterMetersMax} meters</h4>
+                    </div>
+                </div>
+            </div>`;
     }
+    mainDivResults.innerHTML = card;
 }
