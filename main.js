b0af3644ca37093642358ee3d0eb6a9ac714a19c
Jair Hernández <jair_097@hotmail.com>
2022-04-04 17:35:24 -0700

    Update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let header = document.getElementById("header");
 let main = document.getElementById("main");
-let mainDivSearch = document.getElementById("mainDivSearch");
+let headerDivSearch = document.getElementById("headerDivSearch");
 let mainDivResults = document.getElementById("mainDivResults");
 let footer = document.getElementById("footer");
 let auxiliarArray = [];
@@ -28,10 +28,11 @@ function searchAsteroids()
 
 function showAsteroids(array)
 {
-    header.style.display = "none";
+    headerDivSearch.setAttribute("class", "fixed-top");
     console.log(array);
-    mainDivSearch.style.color = "black";
-    mainDivSearch.style.backgroundColor= "white";
+    headerDivSearch.style.outline = "1px solid #CDCDCD"
+    headerDivSearch.style.color = "black";
+    headerDivSearch.style.backgroundColor= "white";
     console.log(array);
     let card = ``;
     for(let i = 0; i < array.length; i++)
@@ -55,7 +56,8 @@ function showAsteroids(array)
             <div class="card"  style="width: 18rem;">
                 <div class="card-body">
                     <div class="titlesOfCard">
-                        <h2 class="card-title">Name: ${name}</h2>
+                        <h5>Name:</h5>
+                        <h2 class="card-title">${name}</h2>
                         <!-- <h4 class="card-subtitle mb-2 text-muted">Close approach date: ${closeApproachDate}</h4> -->
                         <h4 class="card-subtitle mb-2 text-muted">Close approach hour: ${closeApproachHour}</h4>
                         <h4>Relative velocity: ${relativeVelocity} km/s</h4>
